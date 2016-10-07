f29609888781652575c8a223cbb42b490552e3a7
NightMoon032 <407368027@qq.com>
2016-10-08 05:20:30 +0800

    Common Orbwalker StandALone Assembly

---
+++ Common Orbwalker/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Common_Orbwalker
+{
+    using System;
+    using LeagueSharp.Common;
+
+    internal class Program
+    {
+        private static Menu Menu;
+        private static Orbwalking.Orbwalker Orbwalker;
+
+        private static void Main(string[] Args)
+        {
+            CustomEvents.Game.OnGameLoad += OnGameLoad;
+        }
+
+        private static void OnGameLoad(EventArgs Args)
+        {
+            Menu = new Menu("Common Orbwalker", "Common Orbwalker", true);
+
+            var orbMenu = Menu.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
+            Orbwalker = new Orbwalking.Orbwalker(orbMenu);
+
+            Menu.AddToMainMenu();
+        }
+    }
+}
